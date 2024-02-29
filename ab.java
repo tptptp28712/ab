1edc0e5aa7cc234d142b5f6623f4adbd8e2762e9
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-01 00:14:03 +0900

    Update and rename i.java to ab.java

---
+++ ab.java
@@ -0,0 +1,36 @@
+package dog2;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class ab {
+
+    public static void main(String[] args) {
+        // 呼び出し（new した後で、引数へ与えられた値を元に Pokemon が生成されます）
+        Pokemon2 pikachu = new Pokemon2("こんにちは！ここは日本です！","この寿司はうまい","寿司は和食です","今の現在日時は","です");
+        Date date = new Date();
+		SimpleDateFormat jikan = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        // 使用時
+        System.out.println(pikachu.number);
+        System.out.println(pikachu.name);
+        System.out.println(pikachu.category);
+        System.out.println(pikachu.type+jikan.format(date)+pikachu.description);
+        
+
+    }
+}
+class Pokemon2 {
+    // フィールド変数
+    public String number;
+    public String name;
+    public String category;
+    public String type;
+    public String description;
+
+    public Pokemon2(String number, String name, String category, String type, String description) {
+        this.number = number;
+        this.name = name;
+        this.category = category;
+        this.type = type;
+        this.description = description;
+    }
+
+}
