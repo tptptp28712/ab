f6be8b8d713ea7667c99f557c9bcc31684990ec4
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-01 12:20:07 +0900

    Update ab.java

--- ab.java
+++ ab.java
@@ -5,7 +5,7 @@ public class ab {
 
     public static void main(String[] args) {
         // 呼び出し（new した後で、引数へ与えられた値を元に Pokemon が生成されます）
-        Pokemon2 pikachu = new Pokemon2("こんにちは！ここは日本です！","この寿司はうまい","寿司は和食です","今の現在日時は","です");
+        dog.Pokemon2 pikachu = new dog.Pokemon2("こんにちは！ここは日本です！","この寿司はうまい","寿司は和食です","今の現在日時は","です");
         Date date = new Date();
 		SimpleDateFormat jikan = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         // 使用時
