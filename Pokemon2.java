ef708b05aed0617b17342c8566c41c0f2a5b8c35
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-01 12:12:26 +0900

    Create Pokemon2.java

---
+++ Pokemon2.java
@@ -0,0 +1,18 @@
+
+class Pokemon2 {
+    // フィールド変数
+    public String number;
+    public String name;
+    public String category;
+    public String type;
+    public String description;
+
+    public Pokemon2(String number, String name, String category, String type, String description) {
+        this.number = number;
+        this.name = name;
+        this.category = category;
+        this.type = type;
+        this.description = description;
+    }
+
+}
